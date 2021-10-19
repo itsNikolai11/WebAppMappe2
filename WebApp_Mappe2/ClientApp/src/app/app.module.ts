ceefa36fcc1c2f09aa21459ee274943107f8ac70
Nikolai Kopperudmoen <nkopperudmoen@gmail.com>
2021-10-19 14:31:14 +0200

    Endringer i filstruktur

--- WebApp_Mappe2/ClientApp/src/app/app.module.ts
+++ WebApp_Mappe2/ClientApp/src/app/app.module.ts
@@ -1,9 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
