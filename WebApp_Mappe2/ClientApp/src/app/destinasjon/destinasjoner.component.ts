3a2b978f353a355da60e1f263e80f90d6df0f53b
crowbel <magkrstar@gmail.com>
2021-10-27 09:59:51 +0200

    Flyttet destinasjons component ut i egen mappe + oppryddning

---
+++ WebApp_Mappe2/ClientApp/src/app/destinasjon/destinasjoner.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { destinasjon } from "../destinasjon";
+@Component({
+  selector: 'app-root',
+  templateUrl: './destinasjoner.component.html'
+})
+
+export class DestinasjonComponent {
+  public alleDestinasjoner: Array<destinasjon>;
+  public laster: string;
+
+  constructor(private _http: HttpClient) { }
+
+  hentAlleDestinasjoner() {
+    this.laster = "Vennligst vent";
+    this._http.get<destinasjon[]>("api/Destinasjon/")
+      .subscribe(data => {
+        this.alleDestinasjoner = data;
+        this.laster = "";
+      },
+      error => alert(error),
+      () => console.log("ferdig get-/destinajon")
+    );
+  }
+}
+
