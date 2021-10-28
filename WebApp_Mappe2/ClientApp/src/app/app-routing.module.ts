0711143ac7b237d364e682edd5e4b48c4afa8e50
Nikolai Kopperudmoen <nkopperudmoen@gmail.com>
2021-10-28 15:55:35 +0200

    Fikset feil under oppstart

--- WebApp_Mappe2/ClientApp/src/app/app-routing.module.ts
+++ WebApp_Mappe2/ClientApp/src/app/app-routing.module.ts
@@ -1,21 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminpageComponent } from './adminpage/adminpage.component';
-import { DestinasjonComponent } from './destinasjon/destinasjonListe/destinasjoner.component';
-import { destinasjonRediger } from './destinasjon/destinasjonRediger/destinasjonerRediger.component';
 import { LoginComponent } from './login/login.component';
 import { ListeRute } from './rute/liste/listeRute';
-import { AvgangComponent } from './avgang/avgangListe/avgang.component';
-import { LagreRute } from './rute/lagre/lagreRute'; 
+
 const appRoots: Routes = [
   { path: 'adminpage', component: AdminpageComponent },
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'destinasjon', component: DestinasjonComponent },
-  { path: 'redigerDestinasjon', component: destinasjonRediger },
-  { path: 'rute', component: ListeRute },
-  { path: 'avgang', component: AvgangComponent },
-  { path: 'lagreRute', component: LagreRute } 
+  { path: 'rute', component: ListeRute }
 
 ]
 @NgModule({
--- WebApp_Mappe2/ClientApp/src/app/app.module.ts
+++ WebApp_Mappe2/ClientApp/src/app/app.module.ts
@@ -4,15 +4,10 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { Meny } from './meny/meny';
 import { ListeRute } from './rute/liste/listeRute';
 import { AdminpageComponent } from './adminpage/adminpage.component';
 import { AppRoutingModule } from './app-routing.module';
-import { DestinasjonComponent } from './destinasjon/destinasjonListe/destinasjoner.component';
-import { AvgangComponent } from './avgang/avgangListe/avgang.component'
-import { LagreRute } from './rute/lagre/lagreRute'; 
 
 @NgModule({
   declarations: [
@@ -21,16 +16,12 @@ import { LagreRute } from './rute/lagre/lagreRute';
     Meny,
     ListeRute,
     AdminpageComponent,
-    DestinasjonComponent,
-    LagreRute, 
-    AvgangComponent 
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     ReactiveFormsModule,
-    AppRoutingModule,
-    NgbModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
