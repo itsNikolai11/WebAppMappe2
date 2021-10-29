c8142073fc5d8b6ad481af3307fc075cd4ba91d8
bruker <s344066@oslomet.no>
2021-10-29 11:34:41 +0200

    lagt til lagreRute modul

--- WebApp_Mappe2/ClientApp/src/app/app-routing.module.ts
+++ WebApp_Mappe2/ClientApp/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'; 
 import { AdminpageComponent } from './adminpage/adminpage.component';
 import { LoginComponent } from './login/login.component';
 import { ListeRute } from './rute/liste/listeRute';
 import { AvgangComponent } from './avgang/avgangListe/avgang.component';
 import { DestinasjonComponent } from './destinasjon/destinasjonListe/destinasjoner.component';
 import { DestinasjonLagre } from './destinasjon/destinasjonLagre/destinasjonLagre';
+import { LagreRute } from './rute/lagre/lagreRute';
 
 const appRoots: Routes = [
   { path: 'adminpage', component: AdminpageComponent },
@@ -14,7 +15,8 @@ const appRoots: Routes = [
   { path: 'rute', component: ListeRute },
   { path: 'avgang', component: AvgangComponent },
   { path: 'destinasjonListe', component: DestinasjonComponent },
-  { path: 'destinasjonLagre', component: DestinasjonLagre }
+  { path: 'destinasjonLagre', component: DestinasjonLagre },
+  { path: 'lagreRute', component: LagreRute }
   //Husk eks 'rediger/:id' send med f.eks id i rediger, slett kall.
 ]
 @NgModule({
--- WebApp_Mappe2/ClientApp/src/app/app.module.ts
+++ WebApp_Mappe2/ClientApp/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'; 
 import { Meny } from './meny/meny';
 import { ListeRute } from './rute/liste/listeRute';
 import { AdminpageComponent } from './adminpage/adminpage.component';
@@ -11,6 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AvgangComponent } from './avgang/avgangListe/avgang.component';
 import { DestinasjonComponent } from './destinasjon/destinasjonListe/destinasjoner.component';
 import { DestinasjonLagre } from './destinasjon/destinasjonLagre/destinasjonLagre';
+import { LagreRute } from './rute/lagre/lagreRute';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { DestinasjonLagre } from './destinasjon/destinasjonLagre/destinasjonLagr
     AdminpageComponent,
     AvgangComponent,
     DestinasjonComponent,
-    DestinasjonLagre
+    DestinasjonLagre,
+    LagreRute
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
