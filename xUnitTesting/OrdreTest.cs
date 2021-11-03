3d71409262ed12e231195bbe78d5fdfc1511233c
Nikolai Kopperudmoen <nkopperudmoen@gmail.com>
2021-11-03 10:10:08 +0100

    Lagt inn mocking av session i enhetstester. Lagt inn tester på LagreOrdreOk og LagreOrdreIkkeLoggetInn

--- xUnitTesting/OrdreTest.cs
+++ xUnitTesting/OrdreTest.cs
@@ -6,13 +6,23 @@ using Xunit;
 using WebApp_Mappe2.Models;
 using WebApp_Mappe2.DAL;
 using Moq;
+using WebApp_Mappe2.Controllers;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Net;
 
 namespace xUnitTesting
 {
     public class OrdreTest
     {
+        private const string _loggetInn = "loggetInn";
+        private const string _IkkeLoggetInn = "";
+        private readonly Mock<ILogger<OrdreController>> mockLogger = new Mock<ILogger<OrdreController>>();
+        private readonly Mock<HttpContext> mockHttpContext = new Mock<HttpContext>();
+        private readonly MockHttpSession mockSession = new MockHttpSession();
         [Fact]
-        public async Task LagreOrdre()
+        public async Task LagreOrdreOk()
         {
             /*----------Arrange---------*/
             Billett enBillett = new Billett
@@ -23,10 +33,52 @@ namespace xUnitTesting
                 AntallBarn = 1,
                 AntallVoksen = 2
             };
-            var mock = new Mock<IOrdreRepository>();
+            var repositorymock = new Mock<IOrdreRepository>();
+            
+            repositorymock.Setup(b => b.lagreBillett(It.IsAny<Billett>())).ReturnsAsync(true);
+            var ordreController = new OrdreController(repositorymock.Object, mockLogger.Object);
+
+            mockSession[_loggetInn] = _loggetInn;
+            mockHttpContext.Setup(s => s.Session).Returns(mockSession);
+            ordreController.ControllerContext.HttpContext = mockHttpContext.Object;
+
+            /*--------Act--------*/
+
+            var resultat = await ordreController.LagreOrdre(It.IsAny<Billett>()) as OkObjectResult;
+
+            /*--------Assert--------*/
+            Assert.Equal((int) HttpStatusCode.OK, resultat.StatusCode);
+            Assert.Equal("Lagre ok", resultat.Value);
+            
+
+
 
         }
-        public async Task EndreOrdre()
+        [Fact]
+        public async Task LagreOrdreIkkeLoggetInn()
+        {
+            var repositorymock = new Mock<IOrdreRepository>();
+
+            repositorymock.Setup(b => b.lagreBillett(It.IsAny<Billett>())).ReturnsAsync(true);
+            var ordreController = new OrdreController(repositorymock.Object, mockLogger.Object);
+
+            mockSession[_loggetInn] = _IkkeLoggetInn;
+            mockHttpContext.Setup(s => s.Session).Returns(mockSession);
+            ordreController.ControllerContext.HttpContext = mockHttpContext.Object;
+
+            /*--------Act--------*/
+
+            var resultat = await ordreController.LagreOrdre(It.IsAny<Billett>()) as UnauthorizedObjectResult;
+
+            /*--------Assert--------*/
+            Assert.Equal((int)HttpStatusCode.Unauthorized, resultat.StatusCode);
+            Assert.Equal("Ingen tilgang", resultat.Value);
+        }
+        public async Task LagreOrdreIkkeOk()
+        {
+
+        }
+            public async Task EndreOrdre()
         {
             /*----------Arrange---------*/
 
